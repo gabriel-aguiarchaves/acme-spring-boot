95fef7f3618bdfdde545af83395298824008fa64
Gabriel Aguiar <55324924+gabriel-aguiarchaves@users.noreply.github.com>
2021-03-24 18:06:44 -0300

    Add files via upload

---
+++ acme-spring-boot/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (1,'Carlos Barbosa','cgomes@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (2,'Sergio Cortela','cortela@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (3,'Paulo Cabral','paulo@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (4,'Ivo Arruda','ivos@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (5,'Ewerton Digo','digo@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (6,'Gabriel Aguiar','gabriel@gmail.com', '83 9 9126-7778');
